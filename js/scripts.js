f6c45f47c789b8f45872b11f68b8b6c90a4c6523
Tamara McElroy <hello.tamaramc@gmail.com>
2016-04-10 12:45:01 -0400

    revised layout of portfolio page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 
-
+/* Message Fade In */
 $(document).ready(function () {
    $('.welcome').fadeIn(2000);    
 });
 
-$(document).ready(function(){
+/* Smooth Transition to links preceded by hashtag */
+$(document).ready(function() {
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
@@ -17,4 +18,17 @@ $(document).ready(function(){
 	        window.location.hash = target;
 	    });
 	});
+});
+
+/* Fixed Sidebar begins to scroll when it reaches a point midway down the page */
+
+
+$(window).scroll(function() {
+
+
+    if($(".slides").scrollTop() >= 400){
+        $('#sidebar').addClass('scrolling');
+    } else {
+        $('#sidebar').removeClass('scrolling');
+    }
 });
